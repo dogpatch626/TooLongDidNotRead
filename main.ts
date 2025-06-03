fb9b86e1fb6805b99ef504580f661749e7e1c161
dogpatch626 <djpepin1299@gmail.com>
2025-06-03 17:05:24 -0400

    first version commit

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { GoogleGenAI } from '@google/genai';
 
 
@@ -20,29 +20,20 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.client = new GoogleGenAI({ apiKey: this.settings.mySetting });
-
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		const ribbonIconEl = this.addRibbonIcon('sparkle', 'Sample Plugin', (evt: MouseEvent) => {
 			this.client = new GoogleGenAI({ apiKey: this.settings.mySetting });
 			new Notice("Api Key Set");
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.innerHTML = sparkle;
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'analyze-document',
+			name: 'Analyze Document',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				const selection = editor.getSelection();
 				if (selection.length > 0 && this.settings.mySetting) {
@@ -71,27 +62,7 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 				}
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
@@ -105,7 +76,6 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -114,22 +84,6 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
@@ -147,10 +101,10 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Gemini Api Key')
+			.setDesc('This is a seceret obtained through https://ai.google.dev/gemini-api/docs/api-key')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('Enter your api key')
 				.setValue(this.plugin.settings.mySetting || '')
 				.onChange(async (value) => {
 					this.plugin.settings.mySetting = value;
