e222657db0d09703fa18830a23dbed0f001b80cd
dogpatch626 <djpepin1299@gmail.com>
2025-06-12 15:49:01 -0400

    remove innerhtml usage

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { addIcon, App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, setIcon, Setting } from 'obsidian';
 import { GoogleGenAI } from '@google/genai';
 
 
@@ -11,12 +11,13 @@ const DEFAULT_SETTINGS: TooLongDidNotReadPluginSettings = {
 	mySetting: undefined
 }
 const loaderIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)"><stop offset="0" stop-color="#FF156D"></stop><stop offset=".3" stop-color="#FF156D" stop-opacity=".9"></stop><stop offset=".6" stop-color="#FF156D" stop-opacity=".6"></stop><stop offset=".8" stop-color="#FF156D" stop-opacity=".3"></stop><stop offset="1" stop-color="#FF156D" stop-opacity="0"></stop></radialGradient><circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></circle><circle transform-origin="center" fill="none" opacity=".2" stroke="#FF156D" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle></svg>`;
-const sparkle = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkle-icon lucide-sparkle"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/></svg>`;
 const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-x-icon lucide-circle-x"><circle cx="12" cy="12" r="10"/><path d="m15 9-6 6"/><path d="m9 9 6 6"/></svg>`;
 
 export default class TooLongDidNotReadPlugin extends Plugin {
 	settings: TooLongDidNotReadPluginSettings;
 	private client: GoogleGenAI;
+	private pluginIcon = this.addStatusBarItem();
+
 	async onload() {
 		await this.loadSettings();
 		this.client = new GoogleGenAI({ apiKey: this.settings.mySetting });
@@ -25,12 +26,13 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 			this.client = new GoogleGenAI({ apiKey: this.settings.mySetting });
 			new Notice("Api Key Set");
 		});
+		addIcon('loaderIcon', loaderIcon);
+		addIcon('errorIcon', errorIcon);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.innerHTML = sparkle;
 
-		// This adds an editor command that can perform some operation on the current editor instance
+		setIcon(this.pluginIcon, 'sparkle');
+
 		this.addCommand({
 			id: 'analyze-document',
 			name: 'Analyze Document',
@@ -38,7 +40,7 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 				const selection = editor.getSelection();
 				if (selection.length > 0 && this.settings.mySetting) {
 					statusBarItemEl.addClass('status-loader')
-					statusBarItemEl.innerHTML = loaderIcon;
+					setIcon(this.pluginIcon, 'loaderIcon')
 					const response = await this.client.models.generateContent({
 						model: "gemini-2.5-flash-preview-05-20",
 						contents: [
@@ -51,11 +53,11 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 					if (!response) {
 						console.log(response);
 						statusBarItemEl.innerHTML = errorIcon;
-						setTimeout(() => statusBarItemEl.innerHTML = sparkle, 100)
+						setTimeout(() => setIcon(this.pluginIcon, 'errorIcon'), 100)
 
 					} else {
 						editor.replaceRange(JSON.stringify(response.text), editor.getCursor())
-						statusBarItemEl.innerHTML = sparkle;
+						setIcon(this.pluginIcon, 'sparkle')
 
 
 					}
@@ -65,8 +67,7 @@ export default class TooLongDidNotReadPlugin extends Plugin {
 
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
+
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
